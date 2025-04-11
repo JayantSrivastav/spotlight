bec67a81a1b34dab058f8f62ca7bf61dfed1777c
Jayant Srivastava <96934434+JayantSrivastav@users.noreply.github.com>
2025-04-12 02:25:32 +0530

    Project Setup React Native

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Pressable, Text, TouchableOpacity, View } from "react-native";
+import { styles } from "../styles/auth.styles";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Spotlight</Text>
+    </View>
+  );
+}
